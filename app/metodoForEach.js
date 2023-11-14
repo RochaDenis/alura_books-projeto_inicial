bd8350ecf74e03ed0dbd2ccaa99baf8045af87d2
Denis Rocha <dns_rocha@hotmail.com>
2023-11-14 20:51:53 -0300

    filter-avalible-books

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -4,9 +4,10 @@ const elementoParaInserirLivros = document.getElementById('livros')
 function exibirOsLivrosNaTela(listaDeLivros) {
     elementoParaInserirLivros.innerHTML = ''
     listaDeLivros.forEach(livro => {
-        elementoParaInserirLivros.innerHTML += `
-        <div class="livro">
-      <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+      let disponibilidadeLivros = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel'
+      elementoParaInserirLivros.innerHTML += `
+      <div class="livro">
+      <img class="${disponibilidadeLivros}" src="${livro.imagem}" alt="${livro.alt}" />
       <h2 class="livro__titulo">
         ${livro.titulo}
       </h2>
